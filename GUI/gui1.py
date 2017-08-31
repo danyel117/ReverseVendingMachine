9152d398c8fe79d1589a8eda06704705c5200546
Daniel <danielsaldarriaga@outlook.com>
2017-08-30 23:36:34 -0500

    Adicion de contenido de la primera página

--- GUI/gui1.py
+++ GUI/gui1.py
@@ -6,11 +6,35 @@ class MainWindow(Gtk.Window):
 
     def __init__(self):
         Gtk.Window.__init__(self,title='Titulo')
+        self.overlay = Gtk.Overlay()
+        self.add(self.overlay)
+        self.background = Gtk.Image.new_from_file('./Images/Fondo1.jpg')
+        self.overlay.add(self.background)
 
-        #boton
-        self.button=Gtk.Button(label='click here')
+        #grid
+        self.grid=Gtk.Grid(row_spacing=70)
+
+
+        #Label
+        self.label=Gtk.Label()
+        self.label.set_markup('<b><span size="50000" face="verdana" color="#7D2DD7">¡Ingresa tu envase!</span></b>' )
+        self.label.set_halign(Gtk.Align.END)
+        self.grid.attach(self.label,0,0,6,1)
+
+
+        #boton1
+        self.button=Gtk.Button(label='Empezar')
+        self.button.get_child().set_markup('<span size="20000" face="verdana" color="#7D2DD7">¡Comenzar!</span>')
+        #self.button.override_background_color()
+
         self.button.connect("clicked",self.button_clicked)
-        self.add(self.button)
+        self.button.set_size_request(200, 100)
+        self.grid.attach(self.button,2,2,1,1)
+
+        self.overlay.add_overlay(self.grid)
+        self.grid.set_valign(Gtk.Align.CENTER)
+        self.grid.set_halign(Gtk.Align.CENTER)
+
 
     def button_clicked(self,widget):
         print("hola")
